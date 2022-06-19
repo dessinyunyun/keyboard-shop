4d844b408b55f2089df70df519576e6e6a5c7f68
nyun <dessinyunyun@gmail.com>
2022-06-19 17:31:04 +0800

    selesaimi semuanya

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Ui from "./components/DataMapping";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SecondPage from "./components/SecondPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Ui />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            <li>
+              <Link to="/">ke utama</Link>
+            </li>
+            <li>
+              <Link to="/sec">ke sec page</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Ui />} />
+            <Route path="/sec" element={<SecondPage />} />
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
